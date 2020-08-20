9865786cac44172aa38dceaa3e812d4469e1acaf
Jeremy Wood <js.wood206@gmail.com>
2020-08-20 03:59:00 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,14 @@
 require 'pry'
 
 def unsafe?(speed)
-
+	if speed > 60
+		return true
+	elsif speed < 40
+		return true
+	elsif speed >= 40 && speed <= 60
+		return false
+	else
+		return "Incorrect parameter"
 end
 
 
